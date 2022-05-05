375cfe4a5491704a26bda2d3587596eeb6e23f34
Damilola Mohammed <damilolamohammed2@hotmail.com>
2022-05-05 15:16:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
+- Make a selection of which pledge to make
+- See an updated progress bar and total money raised based on their pledge total after confirming a pledge
+- See the number of total backers increment by one after confirming a pledge
+- Toggle whether or not the product is bookmarked
+
+### Screenshot
+
+![](images/Screenshot%20(111).png)
+
+### Links
+
+- Solution URL: [solution](https://github.com/activus-d/frontend-mentor/tree/master/crowdfunding-product-page-main/crowdfunding-product-page-main)
+- Live Site URL: [live site](https://activus-d.github.io/frontend-mentor/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- VanillaJs
+- Web-first workflow
+
+### What I learned
+- Learned how to modify the fill color    svg images.
+- Learned how to make mobile suitable nav bar.
+- Learned how to make progress bar with CSS and JavaScript.
+- learned how querySelectorAll work.
+
+### Continued development
+
+need to continue practice with javaScriot and also make the code object oriented
+
+### Useful resources
+
+- [Geeksforgeeks](https://www.geeksforgeeks.org/create-a-mobile-toggle-navigation-menu-using-html-css-and-javascript/) - This helped me with making mobile friendly nav bar
+- [w3schools](https://www.w3schools.com/jsref/met_document_queryselectorall.asp) - This is with understanding how querySelectorAll works
+
+## Author
+
+- Website - [D.M Oladele](https://activuscode.hashnode.dev/)
+- Twitter - [@activus_d](https://twitter.com/activus_d)
+
---
+++ js/main.js
@@ -0,0 +1,136 @@
+const overlayHide = document.querySelector('.overlayHide')
+const menuIcon = document.querySelector('.menuIcon')
+const iconContainer = document.querySelector('.iconContainer')
+const projectBackPopHide = document.querySelector('.projectBackPopHide')
+const projectBack = document.querySelector('.projectTopConDiv button');
+const projectBackPopUp =  document.querySelector('.projectBackPopUp')
+const bookmarkIcon = document.querySelector('.bookmarkIcon')
+const bookmarkSpan = document.querySelector('.bookmark span')
+const bookmark = document.querySelector('.bookmark')
+const bookmarkPopUp = document.querySelector('.bookmarkPopUp')
+const bookmarkButton = document.querySelector('.bookmarkPopUp button')
+const form1 = document.querySelector('.form1')
+const form2 = document.querySelector('.form2')
+const form3 = document.querySelector('.form3')
+const form4 = document.querySelector('.form4')
+let h41 = document.querySelector('.h41') 
+let h42 = document.querySelector('.h42') 
+let h43 = document.querySelector('.h43') 
+let h44 = document.querySelector('.h44') 
+const closePopUp = document.querySelector('.close')
+const pledgeChoice = document.querySelectorAll('.standPopUp')
+const submit1 = document.querySelector('.submit1')
+const submit2 = document.querySelector('.submit2')
+const submit3 = document.querySelector('.submit3')
+const submit4 = document.querySelector('.submit4')
+
+
+/*SHOW POP-UP OPTIONS WHEN 'BACK THIS PROJECT IS CLICKED' */
+projectBack.addEventListener('click', showprojectPop)
+function showprojectPop() {
+    overlayHide.style.display = "block"
+    projectBackPopUp.style.display = "block"
+}
+
+/*CLOSE POP-UP NOTIFICATION FOR 'BACK THIS PROJECT BUTTON' */
+closePopUp.addEventListener('click', closePop)
+function closePop() {
+projectBackPopHide.style.display = "none"
+overlayHide.style.display = "none"
+}
+
+
+bookmark.addEventListener('click', bookmarked) 
+function bookmarked() {
+    bookmarkIcon.src = ''
+    bookmarkIcon.src = 'images/icon-bookmark-click.svg'
+    bookmarkSpan.textContent = 'Bookmarked'
+    bookmarkSpan.style.color = 'hsl(176, 72%, 28%)'
+    bookmark.style.width = 'auto'
+    overlayHide.style.display = "block"
+    document.querySelector('.bookmarkPopUpHide').style.display = 'block'
+}
+
+
+bookmarkButton.addEventListener('click', bookmarkClose)
+function bookmarkClose() {
+    overlayHide.style.display = "none"
+    document.querySelector('.bookmarkPopUpHide').style.display = "none"
+}
+
+/*SHOW FORM PART FOR EACH POP-UP*/
+h41.addEventListener('click', showForm1)
+function showForm1() {
+    document.querySelector('.listMid1').classList.toggle('removeMidColor')
+    document.querySelector('.standPop1').classList.toggle('standPopBorder')
+    form1.classList.toggle('formHide')
+}
+h42.addEventListener('click', showForm2)
+function showForm2() {
+    document.querySelector('.listMid2').classList.toggle('removeMidColor')
+    document.querySelector('.standPop2').classList.toggle('standPopBorder')
+    form2.classList.toggle('formHide')
+}
+h43.addEventListener('click', showForm3)
+function showForm3() {
+    document.querySelector('.listMid3').classList.toggle('removeMidColor')
+    document.querySelector('.standPop3').classList.toggle('standPopBorder')
+    form3.classList.toggle('formHide')
+}
+h44.addEventListener('click', showForm4)
+function showForm4() {
+    document.querySelector('.listMid4').classList.toggle('removeMidColor')
+    document.querySelector('.specialPop').classList.toggle('standPopBorder')
+    form4.classList.toggle('formHide')
+}
+
+submit1.addEventListener('click', submitOne)
+function submitOne() {
+    projectBackPopHide.style.display = "none"
+    overlayHide.style.display = "none"
+    document.querySelector('.listMid1').classList.toggle('removeMidColor')
+    document.querySelector('.standPop1').classList.toggle('standPopBorder')
+    document.querySelector('.progress1').style.backgroundColor = 'hsl(176, 50%, 47%)'
+}
+submit2.addEventListener('click', submitTwo)
+function submitTwo() {
+    projectBackPopHide.style.display = "none"
+    overlayHide.style.display = "none"
+    document.querySelector('.listMid2').classList.toggle('removeMidColor')
+    document.querySelector('.standPop2').classList.toggle('standPopBorder')
+    document.querySelectorAll('.progress1, .progress2, .progress3').forEach( bar => bar.style.backgroundColor = 'hsl(176, 50%, 47%)' )
+}
+submit3.addEventListener('click', submitThree)
+function submitThree() {
+    projectBackPopHide.style.display = "none"
+    overlayHide.style.display = "none"
+    document.querySelector('.listMid3').classList.toggle('removeMidColor')
+    document.querySelector('.standPop3').classList.toggle('standPopBorder')
+    document.querySelectorAll('.progress1, .progress2, .progress3, .progress4, .progress5, .progress6').forEach( bar => bar.style.backgroundColor = 'hsl(176, 50%, 47%)' )
+}
+submit4.addEventListener('click', submitFour)
+function submitFour() {
+    projectBackPopHide.style.display = "none"
+    overlayHide.style.display = "none"
+    document.querySelector('.listMid4').classList.toggle('removeMidColor')
+    document.querySelector('.standPop4').classList.toggle('standPopBorder')
+    document.querySelectorAll('.progress1, .progress2, .progress3, .progress4, .progress5, .progress6, .progress7, .progress8, .progress9, .progress10').forEach( bar => bar.style.backgroundColor = 'hsl(176, 50%, 47%)' )
+}
+
+
+/*MENU OPTION FOR MOBILE SCREEN SIZE */
+menuIcon.addEventListener('click', menu)
+function menu() {
+    if(document.querySelector('ul').style.visibility === 'visible' ) {
+        document.querySelector('ul').style.visibility = 'hidden'
+        overlayHide.style.display = "none"
+        menuIcon.style.zIndex = '1'
+        menuIcon.src = "images/icon-hamburger.svg"
+    }else {
+        document.querySelector('ul').style.visibility = 'visible'
+        overlayHide.style.display = "block"
+        document.querySelector('header span').style.zIndex = '3'
+        menuIcon.style.zIndex = '3'
+        menuIcon.src = "images/icon-close-menu.svg"
+    }
+}
